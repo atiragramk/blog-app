002b0ebe7aaf10ad1902d5f9bc3cde9e29a5f1d3
Margarita Korotkova <beizmargo@gmail.com>
2022-07-21 19:31:56 +0300

    [fix] fix import issue

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 
-import bookItem from "../../pages/SingleBook/reducer/bookItem";
+import bookItem from "../../pages/BookItem/reducer/bookItem";
 import bookListStatistic from "../../pages/Statistics/reducer/statistic";
-import bookList from "../../pages/Books/reducer/bookList";
+import bookList from "../../pages/BookList/reducer/bookList";
 import pagination from "../../components/Pagination/reducer/pagination";
 import modal from "../modal/reducer/modal";
 
